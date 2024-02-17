b3910f9605bf0488d35ef13ea38329f55be1a9c4
hituji_code <k.m.hituji3340@gmail.com>
2024-02-18 04:03:35 +0900

    共通パーツPC

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -119,7 +119,7 @@
         </svg>
       </div>
     </a>
-    <a href="#" class="button--gold">Read more<div class="button__icon">
+    <a href="#" class="button--ocher">Read more<div class="button__icon">
         <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
           <defs>
             <clipPath id="a">
@@ -139,6 +139,288 @@
         </svg>
       </div>
     </a>
+
+    <!-- カード -->
+    <ol class="activity-cards">
+      <li class="activity-card">
+        <div class="activity-card__img">
+          <img src="wp-content/themes/rinri/assets/images/top/activity1@2x.jpg" alt="">
+        </div>
+        <div class="activity-card__body">
+          <h3 class="activity-card__head">
+            <span class="activity-card__number">#0</span>
+            <span class="activity-card__title">モーニングセミナー</span>
+          </h3>
+          <p class="activity-card__text text">経営者モーニングセミナーは、地元で活躍している方々の講話等を通し、企業のトップとして自らの生き方や会社のあり方を真剣に考えるための場所です。早朝の爽やかな空気を味わいながら明るい一日をスタートさせませんか。</p>
+          <div class="activity-card__button">
+            <a href="#" class="button--red">Read more<div class="button__icon">
+                <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+                  <defs>
+                    <clipPath id="a">
+                      <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+                    </clipPath>
+                  </defs>
+                  <g transform="translate(-638 -975.248)">
+                    <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+                    <g transform="translate(642.742 988.248) rotate(-90)">
+                      <g transform="translate(0 0)" clip-path="url(#a)">
+                        <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                        <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                        <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </a>
+          </div>
+        </div>
+      </li>
+      <li class="activity-card--blue">
+        <div class="activity-card__img">
+          <img src="wp-content/themes/rinri/assets/images/top/activity1@2x.jpg" alt="">
+        </div>
+        <div class="activity-card__body">
+          <h3 class="activity-card__head">
+            <span class="activity-card__number">#0</span>
+            <span class="activity-card__title">モーニングセミナー</span>
+          </h3>
+          <p class="activity-card__text text">経営者モーニングセミナーは、地元で活躍している方々の講話等を通し、企業のトップとして自らの生き方や会社のあり方を真剣に考えるための場所です。早朝の爽やかな空気を味わいながら明るい一日をスタートさせませんか。</p>
+          <div class="activity-card__button">
+            <a href="#" class="button--blue">Read more<div class="button__icon">
+                <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+                  <defs>
+                    <clipPath id="a">
+                      <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+                    </clipPath>
+                  </defs>
+                  <g transform="translate(-638 -975.248)">
+                    <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+                    <g transform="translate(642.742 988.248) rotate(-90)">
+                      <g transform="translate(0 0)" clip-path="url(#a)">
+                        <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                        <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                        <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </a>
+          </div>
+        </div>
+      </li>
+    </ol>
+
+    <!-- カード -->
+    <ul class="member-cards">
+      <li class="member-card">
+        <div class="member-card__img-wrap">
+          <div class="member-card__img">
+            <img src="wp-content/themes/rinri/assets/images/top/member1@2x.jpg" alt="">
+          </div>
+          <p class="member-card__company">株式会社◯◯◯◯◯</p>
+        </div>
+        <div class="member-card__body">
+          <p class="member-card__name">代表取締役　〇〇〇〇</p>
+          <p class="member-card__text text">ここに会社の紹介が入ります。この文章はダミーです。ここに会社の紹介が入ります。</p>
+        </div>
+      </li>
+    </ul>
+
+    <!-- カード -->
+    <ul class="info-cards">
+      <li class="info-card">
+        <div class="info-card__img">
+          <img src="wp-content/themes/rinri/assets/images/top/info1@2x.jpg" alt="" width="" height="" loading="lazy">
+        </div>
+        <h3 class="info-card__title">ここに活動報告のタイトルが入ります。ダミーです。</h3>
+        <div class="info-card__meta">
+          <time class="info-card__date" datetime="2024-01-04">2024.01.04</time>
+          <span class="info-card__category">カテゴリ</span>
+        </div>
+      </li>
+    </ul>
+
+    <!-- カード -->
+    <ul class="seminar-cards">
+      <li class="seminar-card">
+        <div class="seminar-card__head">
+          <div class="seminar-card__title-wrap">
+            <h3 class="seminar-card__title">
+              <span class="seminar-card__title-number">第100回</span><span class="seminar-card__title-text">人生は倫理</span>
+            </h3>
+          </div>
+          <div class="seminar-card__img">
+            <img src="wp-content/themes/rinri/assets/images/top/seminar1@2x.jpg" alt="" width="" height="" loading="lazy">
+          </div>
+        </div>
+        <dl class="seminar-card__info">
+          <div class="seminar-card__info-wrap text">
+            <dt class="seminar-card__info-term">日時</dt>
+            <dd class="seminar-card__info-description">12月19日（火）</dd>
+          </div>
+          <div class="seminar-card__info-wrap text">
+            <dt class="seminar-card__info-term">会場</dt>
+            <dd class="seminar-card__info-description">下越北倫理法人会</dd>
+          </div>
+          <div class="seminar-card__info-wrap text">
+            <dt class="seminar-card__info-term">講師</dt>
+            <dd class="seminar-card__info-description">田村 征司／新潟県倫理法人会 相談役／(株)トゥルーワークス 会長</dd>
+          </div>
+        </dl>
+      </li>
+    </ul>
+
+    <!-- カード -->
+    <ul class="about-list">
+      <li class="about-item">
+        <h3 class="about-item__title">会場一覧（単会情報）</h3>
+        <div class="about-item__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+            <defs>
+              <clipPath id="a">
+                <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+              </clipPath>
+            </defs>
+            <g transform="translate(-638 -975.248)">
+              <ellipse cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+              <g transform="translate(642.742 988.248) rotate(-90)">
+                <g transform="translate(0 0)" clip-path="url(#a)">
+                  <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                  <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                  <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  <!-- footer -->
+  <footer class="footer">
+    <div class="footer__inner">
+      <div class="footer__wrapper">
+        <div class="footer__left">
+          <div class="footer__logo">
+            <a href="#" class="logo">
+              <div class="logo__icon">
+                <img src="wp-content/themes/rinri/assets/images/common/logo.svg" alt="Logo" width="" height="" loading="lazy">
+              </div>
+              <div class="logo__title">新潟県倫理法人会</div>
+            </a>
+          </div>
+          <address class="footer__address text">
+            〒955-0092<br>
+            新潟県三条市須頃2丁目13番地<br>
+            パークハイツ須頃　202号室-000
+          </address>
+          <dl class="footer__info text">
+            <div class="footer__info-wrap">
+              <dt class="footer__info-term">tel</dt>
+              <dd class="footer__info-description">0256-35-4466</dd>
+            </div>
+            <div class="footer__info-wrap">
+              <dt class="footer__info-term">fax</dt>
+              <dd class="footer__info-description">0256-47-1023</dd>
+            </div>
+            <div class="footer__info-wrap--delta">
+              <dt class="footer__info-term">E-mail</dt>
+              <dd class="footer__info-description">rinri-ken@tiara.ocn.ne.jp</dd>
+            </div>
+          </dl>
+        </div>
+        <div class="footer__right">
+          <div class="footer__nav">
+            <ul class="footer__nav-lists">
+              <li class="footer__nav-list">はじめての方へ
+                <ul class="footer__nav-items text">
+                  <li class="footer__nav-item"><a href="">・倫理法人会ってどんなところ？</a></li>
+                  <li class="footer__nav-item"><a href="">・活動内容</a>
+                  </li>
+                  <li class="footer__nav-item"><a href="">・会員紹介</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="footer__nav-list">会員の方へ
+                <ul class="footer__nav-items text">
+                  <li class="footer__nav-item"><a href="">・お知らせ一覧</a></li>
+                  <li class="footer__nav-item"><a href="">・セミナー案内</a>
+                  </li>
+                  <li class="footer__nav-item"><a href="">・会報</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="footer__nav-list">倫理法人会について
+                <ul class="footer__nav-items text">
+                  <li class="footer__nav-item"><a href="">・会場一覧</a>
+                  </li>
+                  <li class="footer__nav-item"><a href="">・組織・役員</a>
+                  </li>
+                  <li class="footer__nav-item"><a href="">・委員会</a>
+                  </li>
+                  <li class="footer__nav-item"><a href="">・事務局</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div class="footer__buttons">
+            <div class="footer__button">
+              <a href="#" class="button--white">入会のご案内<div class="button__icon">
+                  <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+                    <defs>
+                      <clipPath id="a">
+                        <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+                      </clipPath>
+                    </defs>
+                    <g transform="translate(-638 -975.248)">
+                      <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+                      <g transform="translate(642.742 988.248) rotate(-90)">
+                        <g transform="translate(0 0)" clip-path="url(#a)">
+                          <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                          <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                          <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </a>
+            </div>
+            <div class="footer__button">
+              <a href="#" class="button--ocher">お問い合わせ<div class="button__icon">
+                  <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+                    <defs>
+                      <clipPath id="a">
+                        <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+                      </clipPath>
+                    </defs>
+                    <g transform="translate(-638 -975.248)">
+                      <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+                      <g transform="translate(642.742 988.248) rotate(-90)">
+                        <g transform="translate(0 0)" clip-path="url(#a)">
+                          <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                          <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                          <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="footer__links text">
+            <a href="" class="footer__policy">プライバシーポリシー</a>
+            <a href="" class="footer__link">リンク集</a>
+          </div>
+        </div>
+      </div>
+  </footer>
+
   </div>
 
 </main>
