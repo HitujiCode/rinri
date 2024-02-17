c100dff11e1a0391bca168c7a32853cb0450e4e9
hituji_code <k.m.hituji3340@gmail.com>
2024-02-17 20:47:41 +0900

    button.svg前

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -4,7 +4,45 @@
 
   <!-- 共通 -->
 
-  <div class="test">tet</div>
+  <div class="inner">
+    <div class="section-head">
+      <p class="section-head__en">Beginner’s Guide</p>
+      <h2 class="section-head__ja">はじめての方へ</h2>
+    </div>
+    <p class="text">
+      当サイトをご覧になっている方の中には、
+      倫理法人会に対して、
+      「宗教なのか？」「怖い人がいるのでは？」
+      「強引な勧誘をされたらどうしよう」
+      などなど、さまざまな不安を感じている方もいると思います。
+      実際の会場の雰囲気や活動内容、加入されているメンバーのこと、
+      ありのままをお伝えします。
+    </p>
+    <a href="#" class="button">Read more<div class="button__icon">
+        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+          <defs>
+            <clipPath id="a">
+              <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+            </clipPath>
+          </defs>
+          <g transform="translate(-638 -975.248)">
+            <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+            <g transform="translate(642.742 988.248) rotate(-90)">
+              <g transform="translate(0 0)" clip-path="url(#a)">
+                <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </a>
+    <a href="#" class="button--red"><span>Read more</span></a>
+    <a href="#" class="button--blue"><span>Read more</span></a>
+    <a href="#" class="button--green"><span>Read more</span></a>
+
+  </div>
 
 </main>
 
