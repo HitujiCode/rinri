3c884bd9c2784ad598990e3b612e3e28cafe977c
hituji_code <k.m.hituji3340@gmail.com>
2024-02-17 22:12:45 +0900

    button暫定

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -38,10 +38,107 @@
         </svg>
       </div>
     </a>
-    <a href="#" class="button--red"><span>Read more</span></a>
-    <a href="#" class="button--blue"><span>Read more</span></a>
-    <a href="#" class="button--green"><span>Read more</span></a>
-
+    <a href="#" class="button--red">Read more<div class="button__icon">
+        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+          <defs>
+            <clipPath id="a">
+              <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+            </clipPath>
+          </defs>
+          <g transform="translate(-638 -975.248)">
+            <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+            <g transform="translate(642.742 988.248) rotate(-90)">
+              <g transform="translate(0 0)" clip-path="url(#a)">
+                <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </a>
+    <a href="#" class="button--blue">Read more
+      <div class="button__icon">
+        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+          <defs>
+            <clipPath id="a">
+              <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+            </clipPath>
+          </defs>
+          <g transform="translate(-638 -975.248)">
+            <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+            <g transform="translate(642.742 988.248) rotate(-90)">
+              <g transform="translate(0 0)" clip-path="url(#a)">
+                <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </a>
+    <a href="#" class="button--green">Read more<div class="button__icon">
+        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+          <defs>
+            <clipPath id="a">
+              <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+            </clipPath>
+          </defs>
+          <g transform="translate(-638 -975.248)">
+            <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+            <g transform="translate(642.742 988.248) rotate(-90)">
+              <g transform="translate(0 0)" clip-path="url(#a)">
+                <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </a>
+    <a href="#" class="button--white">Read more<div class="button__icon">
+        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+          <defs>
+            <clipPath id="a">
+              <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+            </clipPath>
+          </defs>
+          <g transform="translate(-638 -975.248)">
+            <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+            <g transform="translate(642.742 988.248) rotate(-90)">
+              <g transform="translate(0 0)" clip-path="url(#a)">
+                <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </a>
+    <a href="#" class="button--gold">Read more<div class="button__icon">
+        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="13" viewBox="0 0 25 13">
+          <defs>
+            <clipPath id="a">
+              <rect width="13" height="20" transform="translate(0 0.258)" fill="none" stroke="#c38b79" stroke-width="1" />
+            </clipPath>
+          </defs>
+          <g transform="translate(-638 -975.248)">
+            <ellipse class="button__svg-ellipse" cx="4.5" cy="5" rx="4.5" ry="5" transform="translate(638 977.248)" fill="#c38b79" opacity="0.5" />
+            <g transform="translate(642.742 988.248) rotate(-90)">
+              <g transform="translate(0 0)" clip-path="url(#a)">
+                <line y1="19.372" transform="translate(6.55 0.201)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M0,0S5.282,1.5,6.12,9.1" transform="translate(0.43 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+                <path d="M6.12,0S.838,1.5,0,9.1" transform="translate(6.55 10.468)" fill="none" stroke="#c38b79" stroke-miterlimit="10" stroke-width="1" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </a>
   </div>
 
 </main>
